64055e83b400e3283a44a944f8e7e50827b3ea02
TBG <lesdocode@gmail.com>
2024-02-14 15:30:46 +0200

    Initial Commit

---
+++ messages.js
@@ -0,0 +1,11 @@
+const crushName = "Thando";
+
+const htmlMessages = [
+  `Happy Valentine's Day, <span id="crushName">${crushName}</span>! Just wanted to
+          say you make my heart skip a beat every time I see you. 😊💕`,
+  "Hope you've been smiling",
+  "And looking fire as always🔥",
+  "Cause why not? You are literally a masterpiece.. intricately crafted with love and beauty 🎨",
+  "Enjoy the rest of your day!",
+  "You can come back to WhatsApp now",
+];
---
+++ script.js
@@ -0,0 +1,41 @@
+const container = document.querySelector(".container");
+const beatingObjects = document.querySelectorAll(".animation-speed");
+
+const hasViewed = localStorage.getItem("viewCount") == 1;
+
+if (hasViewed) {
+  document.querySelector(".container").classList.add("hidden");
+  document.querySelector(".viewed").classList.remove("hidden");
+  document.querySelector(".prompt").classList.add("hidden");
+}
+
+let currentMessageIndex = 0;
+
+function setFullScreen() {
+  document.body.requestFullscreen();
+}
+
+function showNextMessage() {
+  setFullScreen();
+
+  const messagePlaceHolder = document.querySelector(".message");
+  messagePlaceHolder.innerHTML = htmlMessages[currentMessageIndex];
+
+  if (currentMessageIndex < htmlMessages.length - 1) {
+    currentMessageIndex++;
+  } else {
+    localStorage.setItem("viewCount", 1);
+    const clickThroughUrl =
+      "whatsapp://send?phone=27685036260&text=Hi good looking, delete this and type your reply here";
+    window.location.href = clickThroughUrl;
+  }
+
+  console.log(beatingObjects);
+  beatingObjects.forEach((ob) => {
+    ob.classList.add(`animation-speed${currentMessageIndex + 1}`);
+  });
+}
+
+container.addEventListener("click", () => {
+  showNextMessage();
+});
